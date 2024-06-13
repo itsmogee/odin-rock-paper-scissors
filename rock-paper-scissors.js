76904679337a0613a45d5b78ee0b9f6fac91dc92
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-14 01:59:41 +0200

    style(getComputerChoice): remove debugging line

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,6 @@ console.log("Welcome to Rock-Paper-Scissors");
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
-  console.log(choice);
   switch (choice) {
     case 0:
       return "rock";
