bd30dbb3763feb5957280c3cfb2c45260f2dbaac
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-14 01:07:38 +0200

    feat(*): Added function that randomizes the computers output choice for rock-paper-scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,21 @@
 console.log("Welcome to Rock-Paper-Scissors");
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  console.log(choice);
+  switch (choice) {
+    case 0:
+      console.log("Computer : Rock");
+      break;
+    case 1:
+      console.log("Computer : Paper");
+      break;
+    case 2:
+      console.log("Computer : Scissors");
+      break;
+    default:
+      break;
+  }
+}
+
+getComputerChoice();
