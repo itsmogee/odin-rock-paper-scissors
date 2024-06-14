7ffbe1b824273d5d0827722820232448984ff5c3
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-14 02:10:44 +0200

    refactor(logic): Move playRound and score variables into playGame scope

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -24,21 +24,38 @@ function getHumanChoice() {
   return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+// A game consists of 5 rounds
+function playGame() {
 
-// This function will play a single round
-function playRound(humanChoice, computerChoice) {
-  let winner = "draw";
-  if ((humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper') || (humanChoice == 'rock' && computerChoice == 'scissors')) {
-    winner = "human";
-  } else if ((computerChoice == 'paper' && humanChoice == 'rock') || (computerChoice == 'scissors' && humanChoice == 'paper') || (computerChoice == 'rock' && humanChoice == 'scissors')) {
-    winner = "computer";
+  // This function will play a single round
+  playRound = function(humanChoice, computerChoice) {
+
+    // initialize winner, take into account draw scenario 
+    let winner = "draw";
+
+    // Determine the winner using rock-paper-scissors logic
+    if ((humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper') || (humanChoice == 'rock' && computerChoice == 'scissors')) {
+      winner = "human";
+    } else if ((computerChoice == 'paper' && humanChoice == 'rock') || (computerChoice == 'scissors' && humanChoice == 'paper') || (computerChoice == 'rock' && humanChoice == 'scissors')) {
+      winner = "computer";
+    }
+
+    // Update the scores and print the winner to console
+    if (winner == 'human') {
+      console.log('You win! ' + humanChoice + ' beats ' + computerChoice);
+      humanScore += 1;
+    } else if (winner == 'computer') {
+      computerScore += 1;
+      console.log('Sorry you lose! ' + computerChoice + ' beats ' + humanChoice);
+    } else {
+      console.log('It was a draw ' + computerChoice + ' and ' + humanChoice + ' are equal');
+    }
+
   }
-  if (winner == 'human') {
-    console.log('You win! ' + humanChoice + ' beats ' + computerChoice);
-    humanScore += 1;
-  } else if (winner == 'computer') {
+
+  // locally define and initialize the scores
+  let humanScore = 0;
+  let computerScore = 0;
     computerScore += 1;
     console.log('Sorry you lose! ' + computerChoice + ' beats ' + humanChoice);
   } else {
